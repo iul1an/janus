4ed5d9b36eb6a4f3fca03b6859a95ccb9d411047
Iulian Mandache <25257851+iul1an@users.noreply.github.com>
2024-02-14 19:26:12 +0200

    refactor for my needs

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ Janus is a python script that allows you to assume role (retrieve temporary AWS 
 
 Please visit [this blog post](https://www.doit-intl.com/assume-an-aws-role-from-a-google-cloud-without-using-iam-keys/) for the installation.
 
+## Usage
+
+```shell
+$ export AWS_ROLE_ARN=arn:aws:iam::123456789:role/gcp-to-aws
+$ export GCP_TOKEN_AUDIENCE=foo # this value may be used as a condition in the AWS assume role policy
+$ export AWS_SESSION_DURATION=7200 # optional, defaults to 3600s (1h), must have value less than or equal to 43200 (12h)
+$ python janus.py
+```
+
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
--- janus.py
+++ janus.py
@@ -2,7 +2,6 @@
 import requests
 import boto3
 import json
-import sys
 import os
 
 
@@ -22,37 +21,21 @@ def get_metadata(path: str, parameter: str):
         raise SystemExit('Compute Engine meta data error')
 
 
-if __name__ == '__main__':
-    # Get AWS ARN from command line if specified
-    if len(sys.argv) == 2:
-        aws_role_arn = sys.argv[1]
-
-    # Get AWS ARN from env var if specified
-    elif 'AWS_JANUS_ROLE' in os.environ:
-        aws_role_arn = os.environ['AWS_JANUS_ROLE']
-
-    # Fail if both argv and env var configuration failed
-    else:
-        print('Please specify AWS arn role:\neither via env var `AWS_JANUS_ROLE` or \n CLI argument `{} arn:aws:iam::account-id:role/role-name`'.format(sys.argv[0]))
-        exit(1)
-
-    # Get variables from the metadata server
-    try:
-        instance_name = get_metadata('instance', 'hostname')
-    # Cloud Run environment does not return instance name. use 'unknown' instead.
-    except SystemExit:
-        instance_name = 'unknown'
+def get_temp_credentials(aws_role_arn: str, gcp_token_audience: str, session_duration: int):
+    instance_name = get_metadata('instance', 'hostname')
     project_id = get_metadata('project', 'project-id')
     project_and_instance_name = '{}.{}'.format(project_id, instance_name)[:64]
-    token = get_metadata('instance', 'service-accounts/default/identity?format=standard&audience=gcp')
+    token = get_metadata('instance',
+                         'service-accounts/default/identity?format=standard&audience={}'.format(gcp_token_audience))
 
-    # Assume role using gcp service account token
     sts = boto3.client('sts', aws_access_key_id='', aws_secret_access_key='')
 
     res = sts.assume_role_with_web_identity(
         RoleArn=aws_role_arn,
         WebIdentityToken=token,
-        RoleSessionName=project_and_instance_name)
+        RoleSessionName=project_and_instance_name,
+        DurationSeconds=session_duration
+    )
 
     aws_temporary_credentials = {
         'Version': 1,
@@ -62,4 +45,25 @@ if __name__ == '__main__':
         'Expiration': res['Credentials']['Expiration'].isoformat()
     }
 
-    print(json.dumps(aws_temporary_credentials))
+    return aws_temporary_credentials
+
+
+def get_env_variable(var_name: str):
+    var_value = os.getenv(var_name)
+    if var_value is None:
+        raise RuntimeError("The environment variable '{}' is not set.".format(var_name))
+    return var_value
+
+
+if __name__ == '__main__':
+    # Get AWS Role ARN from env
+    arn = get_env_variable('AWS_ROLE_ARN')
+    # Get GCP Token Audience from env
+    audience = get_env_variable('GCP_TOKEN_AUDIENCE')
+    # Get AWS session duration from env
+    duration = int(os.getenv('AWS_SESSION_DURATION', 3600))
+
+    # Get AWS credentials
+    creds = get_temp_credentials(aws_role_arn=arn, gcp_token_audience=audience, session_duration=duration)
+
+    print(json.dumps(creds))
